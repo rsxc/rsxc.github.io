981e56144b361203d084a6527613d728eef94d27
raghavdotcom <drraghavsethi@gmail.com>
2024-01-29 21:23:50 -0500

    adding new style

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -6,7 +6,9 @@ import Home from './components/Home';
 import endpoints from './constants/endpoints';
 import Particles from "react-tsparticles";
 import graphicalOptions from "./theme/particles/graphical.json";
+import soundNotes from "./theme/particles/soundNotes.json";
 import AppContext from './AppContext';
+import { ZIndex } from '@tsparticles/engine';
 
 function MainApp() {
   const [data, setData] = useState(null);
@@ -24,7 +26,8 @@ function MainApp() {
 
   return (
     <div className="MainApp">
-      <Particles options={values.darkMode.value == true ? graphicalOptions : graphicalOptions } />
+
+      <Particles style={{ zIndex: "-100" }} options={values.darkMode.value == true ? graphicalOptions : soundNotes } />
       <NavBarWithRouter />
       <main className="main">
         <Suspense fallback={<FallbackSpinner />}>
